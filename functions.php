601645904bf93602e7d8d1e03dc39ac8d530caa8
Jascha Ehrenreich <jascha@jaeh.at>
2018-06-22 00:47:02 +0200

    indent

--- functions.php
+++ functions.php
@@ -1,14 +1,13 @@
 <?php
 function avatami_enqueue_styles() {
+  $parent_style = 'parent-style'; // This is 'twentyfifteen-style' for the Twenty Fifteen theme.
 
-    $parent_style = 'parent-style'; // This is 'twentyfifteen-style' for the Twenty Fifteen theme.
-
-    wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.less', -1 );
-    wp_enqueue_style( 'child-style',
-      get_stylesheet_directory_uri() . '/style.less',
-      array( $parent_style ),
-      wp_get_theme()->get('Version')
-    );
+  wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.less', -1 );
+  wp_enqueue_style( 'child-style',
+    get_stylesheet_directory_uri() . '/style.less',
+    array( $parent_style ),
+    wp_get_theme()->get('Version')
+  );
 }
 add_action( 'wp_enqueue_scripts', 'avatami_enqueue_styles' );
 
@@ -34,5 +33,3 @@ add_action( 'init', function() {
   // Remove oEmbed-specific JavaScript from the front-end and back-end.
   remove_action('wp_head', 'wp_oembed_add_host_js');
 }, PHP_INT_MAX - 1 );
-
-?>
