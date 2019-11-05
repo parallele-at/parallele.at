be8f5cd93989babde84604695261911b76050d9c
Jascha <jascha@jaeh.at>
2019-11-05 23:13:19 +0100

    remove functionality provided by clean-head

--- functions.php
+++ functions.php
@@ -12,25 +12,3 @@ function avatami_enqueue_styles() {
 
 add_action( 'wp_enqueue_scripts', 'avatami_enqueue_styles', PHP_INT_MIN );
 
-// remove emoji scripts
-remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
-remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
-remove_action( 'wp_print_styles', 'print_emoji_styles' );
-remove_action( 'admin_print_styles', 'print_emoji_styles' );
-
-// remove embed.js but not the rest api and the auto discovery functionality
-// embedding this blog on other blogs ~should~ work
-add_action( 'init', function() {
-  // Remove the REST API endpoint.
-  remove_action('rest_api_init', 'wp_oembed_register_route');
-
-  // Turn off oEmbed auto discovery.
-  // Don't filter oEmbed results.
-  remove_filter('oembed_dataparse', 'wp_filter_oembed_result', 10);
-
-  // Remove oEmbed discovery links.
-  remove_action('wp_head', 'wp_oembed_add_discovery_links');
-
-  // Remove oEmbed-specific JavaScript from the front-end and back-end.
-  remove_action('wp_head', 'wp_oembed_add_host_js');
-}, PHP_INT_MAX - 1 );
