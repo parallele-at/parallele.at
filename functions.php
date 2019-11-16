dfb3fbe823a1bf5dd821d40f5d2b253e80be808f
Jascha <jascha@jaeh.at>
2019-11-16 20:52:21 +0100

    add comments, lint

--- functions.php
+++ functions.php
@@ -1,13 +1,25 @@
 <?php
-function avatami_enqueue_styles() {
-  $parent_style = 'magic-grundstein'; 
+/**
+ * Custom functionality of this theme.
+ *
+ * @package ThemeParallelePolis
+ * @since 0.0.1
+ */
 
-  wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.less' );
-  wp_enqueue_style( 'child-style',
-    get_stylesheet_directory_uri() . '/style.less',
-    array( $parent_style ),
-    wp_get_theme()->get('Version')
-  );
+/**
+ * Enqueue css stylesheets
+ */
+function avatami_enqueue_styles() {
+	$parent_style = 'magic-grundstein';
+
+	wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.less', [], wp_get_theme()->get( 'Version' ) );
+
+	wp_enqueue_style(
+		'child-style',
+		get_stylesheet_directory_uri() . '/style.less',
+		array( $parent_style ),
+		wp_get_theme()->get( 'Version' )
+	);
 }
 
 add_action( 'wp_enqueue_scripts', 'avatami_enqueue_styles', PHP_INT_MIN );
