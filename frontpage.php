2d4cbeb0f791235b33dc516e258a4e227f299ae1
Jascha <jascha@jaeh.at>
2019-12-10 02:58:05 +0100

    update frontpage

--- frontpage.php
+++ frontpage.php
@@ -11,4 +11,5 @@ $context = Timber::context();
 
 $timber_post     = new Timber\Post();
 $context['post'] = $timber_post;
+
 Timber::render( array( 'page-' . $timber_post->post_name . '.twig', 'page-template-frontpage.twig' ), $context );
